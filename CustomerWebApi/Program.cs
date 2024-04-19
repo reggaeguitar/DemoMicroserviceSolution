c14d2524536098012c330dc97df23fe226c8019e
reggaeguitar <joseph.b.brady@gmail.com>
2024-04-18 18:50:01 -0600

    Upgrading to .net 8

--- CustomerWebApi/Program.cs
+++ CustomerWebApi/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};Encrypt=false";
 builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
 /* ===================================== */
 
